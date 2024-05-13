0f1429a81edbb6b0e30f1574b302aa5ac031d71e
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-05-13 17:28:21 +0200

    adding tailwind styles

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Username = () => {
+  return <div className="font-sm hidden font-semibold">Jonas</div>;
+};
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Jonas</p>
+      <Username />
     </header>
   );
 };
